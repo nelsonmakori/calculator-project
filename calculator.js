86740481410e916368ae645b87571015a6ed6936
nelsonmakori <nelsonmakori6@gmail.com>
2023-12-22 18:05:10 -0700

    addig some pictures

--- calculator.js
+++ calculator.js
@@ -1,1 +1,14 @@
-document.getElementById('score').innerText = "nyangau";
+let score ="100";
+
+function displayScores(){
+ let scores = document.querySelectorAll('.score');
+scores[0].innerHTML = '<p>' + score + '</p>';
+}
+
+//displayScores();
+
+function buttons(){
+let seven = document.getElementById('seven')
+seven.addEventListener('click',displayScores)
+}
+ buttons()
