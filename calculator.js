2bc0913298a2692d5a7cc723c1bd4ed30b6d4368
nelsonmakori <nelsonmakori6@gmail.com>
2024-01-22 21:11:50 -0700

    calculator with html css and js
    happy to do this

--- calculator.js
+++ calculator.js
@@ -1,14 +1,40 @@
-let score ="100";
+const display = document.getElementById('display')
+ function displayNumber(input){
+  display.innerText += input ;
 
-function displayScores(){
- let scores = document.querySelectorAll('.score');
-scores[0].innerHTML = '<p>' + score + '</p>';
+ }
+ 
+ function toa(){
+  display.innerText = "";
+ }
+
+ function calculate(){
+  display.innerText = eval(display.innerText)
+ }
+// function deleteNumber(input){
+//  display.value = display.value.toString().slice(0,-1)
+// }
+
+function deleteNumber(input){
+  console.log(display.slice)
+  console.log(input)
+  //display.value -= input
+  display.value = display.value - input;
+
+  // display.value += input
+  display.value = display.value + input
+
+  // m+=y
+  m = m + y;
+
+  // nelson += 1000
+  nelson = nelson + 1000;
+  //x -= 500
+  x = x - 500;
+
 }
-
-//displayScores();
-
-function buttons(){
-let seven = document.getElementById('seven')
-seven.addEventListener('click',displayScores)
-}
- buttons()
+const monthlyrRent = 2000;
+const yearRent = monthlyrRent * 12;
+const  salary = yearRent - 4800;
+console.log(yearRent)
+console.log(salary)
