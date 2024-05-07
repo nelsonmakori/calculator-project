ebc451b952e22d33943b69cc76c06fe47be36a4d
nelsonmakori <nelsonmakori6@gmail.com>
2024-05-06 21:55:27 -0600

    dellte and clear
    added clearAll function and delete one number at a time

--- calculator.js
+++ calculator.js
@@ -1,40 +1,32 @@
+
+
 const display = document.getElementById('display')
  function displayNumber(input){
   display.innerText += input ;
 
  }
  
- function toa(){
+ function clearAll(){
   display.innerText = "";
  }
 
  function calculate(){
   display.innerText = eval(display.innerText)
  }
-// function deleteNumber(input){
-//  display.value = display.value.toString().slice(0,-1)
-// }
 
-function deleteNumber(input){
-  console.log(display.slice)
-  console.log(input)
-  //display.value -= input
-  display.value = display.value - input;
+ function deleteNumber(){
+  display.innerText = display.innerText.slice(0,-1)
+  }
 
-  // display.value += input
-  display.value = display.value + input
 
+
+  
   // m+=y
-  m = m + y;
+  //m = m + y;
 
   // nelson += 1000
-  nelson = nelson + 1000;
+ // nelson = nelson + 1000;
   //x -= 500
-  x = x - 500;
+ // x = x - 500;
 
-}
-const monthlyrRent = 2000;
-const yearRent = monthlyrRent * 12;
-const  salary = yearRent - 4800;
-console.log(yearRent)
-console.log(salary)
+
